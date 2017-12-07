a6e26f9a01a927afb82bd24846bc45e487020be1
Thanh Dao <coelsman@gmail.com>
2017-12-07 16:19:24 +0700

    - Gruntfile: add concat task  - Update layout

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,30 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package-lock.json'),
+		concat: {
+			dist: {
+				src: [
+					'public/src/scss/variables.scss',
+					'public/src/scss/mixins.scss',
+					'public/src/scss/main.scss',
+					'public/src/scss/responsive.scss'
+				],
+				dest: 'public/src/scss/build.scss',
+			}
+		},
 		sass: {
 			dist: {
 				options: {
 					style: 'expanded',
 				},
 				files: {
-					'public/css/style.css': 'public/src/scss/main.scss',
+					'public/css/style.css': 'public/src/scss/build.scss'
 				}
 			}
 		},
 	});
+	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-sass');
 
-	grunt.registerTask('default', ["sass"]);
+	grunt.registerTask('default', ["concat", "sass"]);
 };
